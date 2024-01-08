03cf2a5b6df667749a55dc37f238ca37e21fd276
tomilin229 <tomilin229@gmail.com>
2024-01-09 01:08:07 +0300

    bug fix

--- src/pages/photo.tsx
+++ src/pages/photo.tsx
@@ -9,7 +9,7 @@ export const PhotoCard = (props: IPhoto) => {
   const { url, title, id } = props
 
   return (
-    <Link href={router.pathname + '/' + id} id={"photo" + id}>
+    <Link href={router.pathname + '/' + id} id={"photo"}>
       <div className="mt-2 flex justify-between w-80 rounded-md bg-blue-200 p-2 gap-2">
         <img width={120} height={120} src={url} alt={'post'}/>
         <p className="font-semibold text-gray-900">{title}</p>
--- src/pages/post.tsx
+++ src/pages/post.tsx
@@ -9,7 +9,7 @@ export const PostCard = (props: IPost) => {
   const { body, title, id } = props
 
   return (
-    <Link href={router.pathname + '/' + id} id={"post" + id}>
+    <Link href={router.pathname + '/' + id} id={"post"}>
       <div className="mt-4 flex w-100 rounded-md gap-10 bg-blue-200 p-2 border-r-4">
         <div>
           <span className="ml-auto font-bold text-gray-900">{title}</span>
